05f2997cf35dfba631b00af0bfab4473349f6f01
NiranjanReddy-Aare <303467@ust.com>
2025-12-05 14:14:29 +0530

    Ems

--- database.py
+++ database.py
@@ -1,7 +1,15 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+import pymongo
+import gridfs
 
 client = AsyncIOMotorClient("mongodb+srv://303391_db_user:5IhrghdRaiXTR22b@cluster0.i0ih74y.mongodb.net/?appName=Cluster0")
 db = client.talent_management
+
+sync_client = pymongo.MongoClient("mongodb+srv://303391_db_user:5IhrghdRaiXTR22b@cluster0.i0ih74y.mongodb.net/?appName=Cluster0")
+sync_db = sync_client.talent_management
+
+# Use synchronous GridFS with pymongo
+fs = gridfs.GridFS(sync_db,collection="files")
 
 collections = {
     "employees": db.employees,
@@ -10,5 +18,14 @@ collections = {
     "refresh_tokens": db.refresh_tokens,
     "audit_logs": db.audit_logs,
     "block_list_tokens":db.block_list_tokens,
-    "resource_request":db.resource_request
+    "resource_request":db.resource_request,
+    "files":db.files.files
 }
+
+def get_gridfs():
+    return fs
+ 
+applications = db.applications
+resource_request= db.resource_request
+employees = db.employees
+files=db.files.files
